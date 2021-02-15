88552d905f4e1bd162140390580138e7ba0f01c8
mariamcobo <mariamcobo@hotmail.com>
2021-02-15 18:43:33 +0100

    Agregar archivos de proyecto.

---
+++ Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class Figuras1
+{
+	public Class1()
+	{
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FIGURAS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
