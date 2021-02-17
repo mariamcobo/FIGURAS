a0a6f1ea6759446924c9b78922489b33586d0c1b
mariamcobo <78874826+mariamcobo@users.noreply.github.com>
2021-02-17 18:26:03 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -6,7 +6,44 @@ namespace FIGURAS
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            try 
+            {
+                int n = 1;
+
+                for(int i = 0; i<n; i++)
+                {
+                    Console.WriteLine("¿Desea utilizar la aplicación? <S/N> \n");
+                    string valor = Console.ReadLine();
+
+                    if (valor.ToUpper() == "S")
+                    {
+                        Console.WriteLine("Escribe la figura de la que desea calcular el Área: \n" +
+                    "1. Cuadrado \n" + "2. Rectángulo \n" + "3. Círculo \n");
+                        string opcion = "";
+                        figuras figuras = new figuras();
+                        switch (opcion)
+                        {
+                            case "1": figuras.areaCuadrado(); break;
+                            case "2": figuras.areaRectangulo(); break;
+                            case "3": figuras.areaCirculo(); break;
+                            default: Console.WriteLine("Opción elegida no válida"); break;
+                        }
+                        n++;
+                    }
+
+                    else if (valor == "N" || valor == "n") //Otra forma sin el "ToUpper"
+                    {
+                        return; //Termina la función principal y se termina la ejecución en curso
+                    }
+                }
+                Console.ReadKey();
+            }
+            
+            catch (Exception ex)
+            {
+                Console.WriteLine("La aplicación ha fallado. Contacte con su técnico" + ex.Message);
+            }   
         }
     }
 }
---
+++ figuras.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FIGURAS
+{
+	public class figuras
+	{
+		//public decimal area { get; set; } Otra forma que no usaremos
+		double area = 0, lado = 0, ancho = 0, largo = 0, radio = 0;
+
+		public void areaCuadrado()
+		{
+			Console.WriteLine("Escribe el lado ");
+			lado = Convert.ToDouble(Console.ReadLine());
+			area = lado * lado;
+			Console.WriteLine("El resultado es: " + area);
+		}
+
+		public void areaRectangulo()
+		{
+			Console.WriteLine("Escribe el ancho del rectángulo ");
+			ancho = Convert.ToDouble(Console.ReadLine());
+			Console.WriteLine("Escribe el largo del rectángulo ");
+			largo = Convert.ToDouble(Console.ReadLine());
+			area = ancho * largo;
+			Console.WriteLine("El resultado es: " + area);
+		}
+
+		public void areaCirculo()
+		{
+			Console.WriteLine("Escribe el radio del círculo ");
+			radio = Convert.ToDouble(Console.ReadLine());
+
+			area = Math.PI * (radio * radio);
+			Console.WriteLine("El resultado es: " + area);
+		}
+	}
+}
